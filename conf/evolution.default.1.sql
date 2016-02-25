d968493004fd1de12d4929432b7dc9be6738f69d
srijeshgit <khanalsir.srijesh@gmail.com>
2016-02-25 12:55:49 +0545

    my first app

---
+++ conf/evolution.default.1.sql
@@ -0,0 +1,10 @@
+
+
+
+CREATE TABLE SignUp (
+   firstname char(200) NOT NULL ,
+   lastname  char(200) NOT NULL,
+   phone1    varchar(200) NOT NULL,
+   phone2    varchar(200) ,
+   email     varchar(200) NOT NULL
+);
